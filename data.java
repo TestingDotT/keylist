4f3e8ad4e49502f850289da7db306286ea38c633
TestingDotT <123202966+TestingDotT@users.noreply.github.com>
2023-01-20 13:50:03 -0600

    Update data.java

--- data.java
+++ data.java
@@ -1,3 +1,9 @@
-        // Key list (Updated)
-        // Index data
-        String[] keylist = {"key1", "key2", "key3", "key4"};
+// Data
+
+public class data {
+        public static void main(String[] args) {
+                // Key list (Updated)
+                // Index data
+                String[] keylist = {"key1", "key2", "key3", "key4"};
+        }
+}
