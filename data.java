43d8f7406bfb66a0560c1d34dc184b3a632e9a93
TestingDotT <123202966+TestingDotT@users.noreply.github.com>
2023-01-23 13:15:13 -0600

    Update data.java

--- data.java
+++ data.java
@@ -5,5 +5,13 @@ public class data {
                 // Key list (Updated)
                 // Index data
                 String[] keylist = {"key1", "key2", "key3", "key4"};
+                
+                // Check
+                if (userkey.equals(keylist[0])) {
+                        // Output test
+                        System.out.println("Welcome!");
+                } else {
+                        System.out.println("Error. Not welcome!");
+                }
         }
 }
